c644e17d862691f44e58d83bea37c0000b9b06ed
Tristan Dinh <92371193+TekTristan@users.noreply.github.com>
2023-07-02 13:42:23 -0700

    started rpj project

---
+++ tek.js
@@ -0,0 +1,68 @@
+getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+
+let playerCounter = 0;
+let computerCounter = 0;
+
+playRound = (playerSelection, computerSelection) => {
+    if (playerSelection === computerSelection) {
+        return 'It\'s a tie!';
+    } else if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            computerCounter += 1;
+            return 'You lose! Paper beats Rock';
+        } else {
+            playerCounter += 1;
+            return 'You win! Rock beats Scissors';
+        }
+    } else if (playerSelection === 'paper') {
+        if (computerSelection === 'scissors') {
+            computerCounter += 1;
+            return 'You lose! Scissors beats Paper';
+        } else {
+            playerCounter += 1;
+            return 'You win! Paper beats Rock';
+        }
+    } else if (playerSelection === 'scissors') {
+        if (computerSelection === 'rock') {
+            computerCounter += 1;
+            return 'You lose! Rock beats Scissors';
+        } else {
+            playerCounter += 1;
+            return 'You win! Scissors beats Paper';
+        }
+    }
+}
+
+game = (playerCounter, computerCounter) => {
+    while (playerCounter < 5 && computerCounter < 5) {
+        let playerSelection = prompt('Rock, Paper or Scissors?');
+        playerSelection = playerSelection.toLowerCase();
+        const computerSelection = getComputerChoice();
+        
+        console.log(playRound(playerSelection, computerSelection));
+        console.log('Player score: ' + playerCounter + ', Computer score: ' + computerCounter);
+    } 
+
+    if (playerCounter === 5) {
+        console.log('You win!');
+    } else if (computerCounter === 5) {
+        console.log('You lose!');
+    }
+}
+
+game();
+
+
+
